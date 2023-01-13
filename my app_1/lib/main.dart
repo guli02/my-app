5f68e1e1854fe177e0ea344956af26dd4a2fde35
Elebesova Gulzat <88238675+guli02@users.noreply.github.com>
2023-01-13 17:20:30 +0600

    guli02
    my app_1

---
+++ my app_1/lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        "Hello Flutter",
+        style: TextStyle(fontSize: 30),
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
